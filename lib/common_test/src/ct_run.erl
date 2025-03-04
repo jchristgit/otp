c00f7721d0bb2b294c9418d75f9a887f2e5641f1
Johannes Christ <jc@jchri.st>
2025-03-04 21:10:40 +0100

    ct: Remove long deprecated support for sequences/0 callback
    This has printed a deprecation warning for as long as my git history
    goes back (see diff in `ct_util`). I have done a code search on GitHub
    [1] and with the exception of the OTP repository the only repositories
    which had anything specified in here were last updated in 2011.
    
    [1]: https://github.com/search?q=language%3Aerlang+%2F%5Esequences.*%5Cn.*%5C%5B.%2B%5C%5D%2F+path%3A*_SUITE.erl+&type=code&p=1

--- lib/common_test/src/ct_run.erl
+++ lib/common_test/src/ct_run.erl
@@ -2212,7 +2212,6 @@ do_run_test(Tests, Skip, Opts0) ->
 	    Suites1 = delete_dups(Suites),
 	    NoOfTests = length(Tests),
 	    NoOfSuites = length(Suites1),
-	    ct_util:warn_duplicates(Suites1),
 	    {ok,Cwd} = file:get_cwd(),
 	    io:format("~nCWD set to: ~tp~n", [Cwd]),
 	    if NoOfCases == unknown ->
@@ -3213,23 +3212,7 @@ get_all_testcases(Suite) ->
 	{error,_Reason} = Error ->
 	    Error;
 	SuiteCases ->
-	    Cases = [C || {_S,C} <- SuiteCases],
-	    try Suite:sequences() of
-		[] ->
-		    Cases;
-		Seqs ->
-		    TCs1 = lists:flatten([TCs || {_,TCs} <- Seqs]),
-		    lists:reverse(
-		      lists:foldl(fun(TC, Acc) ->
-					  case lists:member(TC, Acc) of
-					      true  -> Acc;
-					      false -> [TC | Acc]
-					  end
-				  end, [], Cases ++ TCs1))
-	    catch
-		_:_ ->
-		    Cases
-	    end
+	    [C || {_S,C} <- SuiteCases]
     catch
 	_:Error ->
 	    {error,Error}
